60e3926a0be45ef58d4eafcdb822770be5f96cd9
AbbyShancock <asaucedo44@cps.edu>
2024-11-18 14:34:16 -0600

    Create max_min.py

---
+++ max_min.py
@@ -0,0 +1,24 @@
+ #HOW TO GET MAX VALUE
+
+number_list = [44542247/2, 21310/5, 2134747*33, 44556475, 121676, 6654067, 353254, 123134, 55**12, 611**5]
+maximum_value = max(number_list)
+print(maximum_value)
+
+
+# #max value - min value
+
+number_list = [44542247, 21310, 2134747, 44556475, 121676, 6654067, 353254, 123134, 552512, 611665]
+number_range = max(number_list) - min(number_list)
+print(number_range)
+
+
+
+# #min age and last name alphabetically
+
+dictionary_ages = {"Tony":55, "Paulie":42, "Meadow":78, "Vito":44, "Ralph":24, "Sarah":35, "Evan":19, "Jean":2, "Ned":49}
+
+minimum_age = min(dictionary_ages.values())
+last_name = max(dictionary_ages.keys())
+
+print(minimum_age)  # Minimum value from the dictionary
+print(last_name)    # Last name alphabetically
