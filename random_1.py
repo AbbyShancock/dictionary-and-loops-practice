418dca36dd60a5040ca858d0a2551007ceaedefc
AbbyShancock <asaucedo44@cps.edu>
2024-11-18 14:34:49 -0600

    Create random_1.py

---
+++ random_1.py
@@ -0,0 +1,20 @@
+#RANDOM PRACTICE:
+import random
+
+
+random_number = random.randint(1, 10)
+print(random_number)
+
+
+
+
+random_number2 = random.random()
+print(random_number2)
+
+
+
+
+
+names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
+raffle = random.choice(names)
+print(raffle) #This will print a random name from the names list
